1895cc146e0274a277be3f4ff2e6d3db397cbdfd
Sanni Oluwafikayo <sanni@exqu.co>
2023-05-01 06:56:55 +0100

    chore: fix otp expiry

--- src/models/otp.model.ts
+++ src/models/otp.model.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 import { confirmUser as otpUser } from "../types/confirm.type";
-import { generateConfirmationExpiry, generateSixDigitToken } from "../utils/users.utils.string";
+import { generateOTPExpiry, generateSixDigitToken } from "../utils/users.utils.string";
 
 // const uuid = require('uuid/v1');
 const schema = new mongoose.Schema<otpUser>({
     created: {type: Date, 'default': Date.now},
     token: {type: String, 'default': generateSixDigitToken},
     status: {type: Boolean, 'default': false},
-    expiry: {type: Date, 'default': generateConfirmationExpiry},
+    expiry: {type: Date, 'default': generateOTPExpiry},
     user_id: {type: mongoose.Schema.Types.ObjectId, ref: "Users"}
 })
 
