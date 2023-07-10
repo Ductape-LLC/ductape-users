872cc09371377cefa62742cc88e363b64e93d0cc
Sanni Oluwafikayo <sanni@exqu.co>
2023-07-10 19:12:24 +0100

    chore: cleanup error handling

--- src/utils/confirm.utils.read.ts
+++ src/utils/confirm.utils.read.ts
@@ -1,13 +1,13 @@
 import { PipelineStage } from "mongoose";
 import { model } from "../models/confirm.model";
 import { confirmUser } from "../types/confirm.type";
-import { handleError } from "../errors/errors";
+import { UserError, handleError } from "../errors/errors";
 
 export const fetchConfirm = async (get: PipelineStage[]): Promise<confirmUser> => {
     try{
         const data = await model.aggregate(get);
 
-        if(!data.length) throw "Confirm credentials not found";
+        if(!data.length) throw new UserError("Confirm credentials not found", 400);
 
         return data[0];
     } catch(e){
--- src/utils/otp.utils.read.ts
+++ src/utils/otp.utils.read.ts
@@ -1,13 +1,13 @@
 import { PipelineStage } from "mongoose";
 import { model } from "../models/otp.model";
 import { confirmUser as otpUser } from "../types/confirm.type";
-import { handleError } from "../errors/errors";
+import { UserError, handleError } from "../errors/errors";
 
 export const fetchOTP = async (get: PipelineStage[]): Promise<otpUser> => {
     try{
         const data = await model.aggregate(get);
 
-        if(!data.length) throw "OTP credentials not found";
+        if(!data.length) throw new UserError("OTP credentials not found", 400);
 
         return data[0];
     } catch(e){
