65fc6afea579a435e568f76f5bc647ebfc93fe6c
Christopher Wuni <christdam55@gmail.com>
2024-07-04 23:03:41 +0100

    url rewrite and health check url done

--- src/app.ts
+++ src/app.ts
@@ -4,34 +4,43 @@ import * as dotEnv from 'dotenv';
 import router from './commons/routesConfig';
 import cors from 'cors';
 import { UserError, sendErrorResponse } from './errors/errors';
+import { urlRewrite } from './middleware/url-rewrite';
 
 const app = express();
 const port = process.env.PORT || 8002;
 
 dotEnv.config();
+
+app.use(urlRewrite);
+
 // Bodyparser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const allowedOrigins = ['http://localhost:3000','https://ductape-hwyp.vercel.app',];
+const allowedOrigins = ['http://localhost:3000', 'https://ductape-hwyp.vercel.app'];
 
 const options: cors.CorsOptions = {
-  origin: allowedOrigins
+  origin: allowedOrigins,
 };
 
 app.use(cors(options));
 
 app.use(router);
+
+app.get('/api/v1/status', (_req, res) => {
+  res.send(`ductape-users-api is healthy`);
+});
+
 mongoose.connect(process.env.DB_HOST as string).catch((e) => {
-  if(process.env.NODE_ENV !== "production") console.log(e);
+  if (process.env.NODE_ENV !== 'production') console.log(e);
 });
 
 mongoose.connection.on('open', () => {
-  if(process.env.NODE_ENV !== "production") console.log('Mongoose Connection');
+  if (process.env.NODE_ENV !== 'production') console.log('Mongoose Connection');
 });
 
 app.listen(port, () => {
-  if(process.env.NODE_ENV !== "production") console.log(`ductape-users-api app is running on port ${port}.`);
+  if (process.env.NODE_ENV !== 'production') console.log(`ductape-users-api app is running on port ${port}.`);
 });
 
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
---
+++ src/middleware/url-rewrite.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const urlRewrite = async (
+  req: Request,
+  _: Response,
+  next: NextFunction
+) => {
+  if (req.url.startsWith('/users')) {
+    req.url = req.url.replace('/users', '');
+  }
+  next();
+};
