885a027d8bc743836d9613f955564872d3fd383a
oceanencrypt <oceanencrypt@gmail.com>
2024-12-13 05:12:43 +0100

    Removed Validator error message for password mismatch

--- src/validators/users.validator.change-password.ts
+++ src/validators/users.validator.change-password.ts
@@ -2,17 +2,9 @@ import joi from 'joi';
 import { change_password } from '../types/users.type';
 
 const changePasswordSchema = joi.object<Partial<change_password>>({
-    oldPassword: joi.string().required(),
-    newPassword: joi.string()
-        .min(8)
-        .max(64)
-        .required(),
-    confirmNewPassword: joi.string()
-        .valid(joi.ref('newPassword'))
-        .required()
-        .messages({
-            'any.only': 'Passwords must match'
-        })
+  oldPassword: joi.string().required(),
+  newPassword: joi.string().min(8).max(64).required(),
+  confirmNewPassword: joi.string().min(8).max(64).required(),
 });
 
 export default changePasswordSchema;
