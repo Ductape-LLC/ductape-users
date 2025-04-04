5bf7ab3a3f66cfceaba6971437816569eb3f06a3
oceanencrypt <oceanencrypt@gmail.com>
2025-04-04 10:28:13 +0100

    Refactor Paystack customer model and add billing information handling; implement create and update validators for billing info

--- src/models/paystack.model.ts
+++ src/models/paystack.model.ts
@@ -1,46 +1,84 @@
-import mongoose from "mongoose";
-import { UserStatus } from "../events/user.events.types";
-import { PaystackCustomer } from "../types/paystack.type";
+import mongoose from 'mongoose';
+import { UserStatus } from '../events/user.events.types';
+import { PaystackCustomer } from '../types/paystack.type';
 
-const schema = new mongoose.Schema<PaystackCustomer>({
+const schema = new mongoose.Schema<PaystackCustomer>(
+  {
     user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'users',
-        required: true,
-        unique: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+      unique: true,
     },
     customer_code: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: false,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: false,
     },
     authorization: {
-        authorization_code: String,
-        card_type: String,
-        last4: String,
-        exp_month: String,
-        exp_year: String,
-        bin: String,
-        bank: String,
-        channel: String,
-        signature: String,
-        reusable: Boolean,
-        country_code: String
+      authorization_code: String,
+      card_type: String,
+      last4: String,
+      exp_month: String,
+      exp_year: String,
+      bin: String,
+      bank: String,
+      channel: String,
+      signature: String,
+      reusable: Boolean,
+      country_code: String,
     },
     status: {
-        type: String,
-        enum: UserStatus,
-        required: true
+      type: String,
+      enum: UserStatus,
+      required: false,
+      default: UserStatus.ACTIVE,
     },
     domain: String,
     metadata: {
-        type: Map,
-        of: mongoose.Schema.Types.Mixed
-    }
-}, { timestamps: true });
+      type: Map,
+      of: mongoose.Schema.Types.Mixed,
+    },
+    billingInformation: {
+      firstName: {
+        type: String,
+        required: false,
+      },
+      lastName: {
+        type: String,
+        required: false,
+      },
+      addressLine1: {
+        type: String,
+        required: false,
+      },
+      addressLine2: {
+        type: String,
+        required: false,
+      },
+      city: {
+        type: String,
+        required: false,
+      },
+      country: {
+        type: String,
+        required: false,
+      },
+      stateProvince: {
+        type: String,
+        required: false,
+      },
+      postalZipCode: {
+        type: String,
+        required: false,
+      },
+    },
+  },
+  { timestamps: true },
+);
 
 export const PaystackCustomerModel = mongoose.model('paystack_customer', schema);
